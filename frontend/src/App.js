cc9bdaffe686cc03e5f715a54b1eb2c8a0881604
jiwonchoe12 <belle021202@naver.com>
2023-06-10 16:47:09 +0900

    changed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -56,7 +56,7 @@ function App() {
             <li key={index}>{list.value} </li>
           ))}
           <br />
-            안녕하세요.
+            Notice Board
           <form className="example" onSubmit={submitHandler}>
             <input
               type="text"
